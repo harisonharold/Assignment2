07bf87a520582e2c18799e633e39a84fc6ce4948
harisonharold <harisonharold55@gmail.com>
2023-03-19 00:49:13 -0400

    firstcommit

---
+++ application.py
@@ -0,0 +1,6 @@
+import product
+
+class Application:
+        print('Welcome to Programming Principles Sample Product Inventory')
+        
+product.Product(input("Please enter the Product code:"),input('Please enter the Product Name:'),input('Please enter the current stock: '),input('Please enter the Product Sale Price: '),input('Please enter the Product Manufacture Cost: '),input('Please enter estimated monthly production: '))
---
+++ product.py
@@ -0,0 +1,11 @@
+class Product:
+    def __init__(self, pcode, pname, cstock, psp, pmc, monp):
+        self.pcode = pcode
+        self.pname = pname
+        self.cstock = cstock
+        self.psp = psp
+        self.pmc = pmc
+        self. monp = monp
+    def page(self):
+
+        return 
